a55f7b42cea35c9224dc0b56d0caf3bb6f4b1308
David Lewitan <dovidlwtn@gmail.com>
2017-01-31 03:13:29 +0000

    created skeleton for project. with README bin and lib

---
+++ README.md
@@ -0,0 +1,1 @@
+# rubydoc-scan-cli-app
---
+++ lib/command_line_interface.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'colorize'
+
+class CommandLineInterface
+  BASE_PATH = "http://ruby-doc.org/core"
+
+  def initialize(keyword)
+    @@keyword = keyword
+    @@data = Doc_data.new
+  end
+
+  def run
+    collect_doc_refs
+    organize_data
+  end
+
+  def collect_doc_refs
+    #scrape doc for references to keyword
+  end
+
+  def organize_data
+    #@@data.organize
+  end
+
+end
