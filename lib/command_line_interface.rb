b03fde7cd15996b6f98eccb784ea68590bee1231
David Lewitan <dovidlwtn@gmail.com>
2017-02-01 02:01:03 +0000

    moved path directly into the scraper and out of the comand_line class. started outlining the Scrape class

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,22 +1,22 @@
 require 'colorize'
 
 class CommandLineInterface
-  BASE_PATH = "http://ruby-doc.org/core"
 
   def initialize(keyword)
-    @@keyword = keyword
-    @@data = Documentation_data.new
+    @keyword = keyword
+    @data = Documentation_data.new
   end
 
   def run
     collect_doc_refs
     organize_data
     #dev
-    puts @@keyword
+    puts @keyword
   end
 
   def collect_doc_refs
     #scrape doc for references to keyword
+    Scrape.scrape_the_doc(@keyword)
   end
 
   def organize_data
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,10 @@
+require 'open-uri'
 require 'nokogiri'
 
 class Scraper
+  BASE_PATH = "http://ruby-doc.org/core"
 
+  def self.scrape_the_doc
+
+  end
 end
