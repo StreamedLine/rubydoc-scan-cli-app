ad37cfc7d628fbf80e0ac5449148d9feaa26051e
David Lewitan <dovidlwtn@gmail.com>
2017-02-06 04:24:45 +0000

    scrape appropiate info from doc

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Accept a keyword and searches for it in the ruby doc.
 
 will return the link(s) most closely related to word.
+
+link can be viewd in the command line or directly in the browser.
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,15 +15,47 @@ class Scraper
     results
   end
 
+  def self.traverseIt(chunk)
+    chunk = chunk.children[0]
+    while chunk.next
+      if chunk.name == 'div'
+        traverseIt(chunk)
+        chunk = chunk.next
+        next
+      end
+      if chunk.name == 'pre'
+        puts '========================================'.colorize(:color => :red)
+        puts chunk.inner_text.colorize(:color => :yellow).gsub('\r' , "").strip
+      else
+        if chunk.inner_text.split(" ").uniq.count < 2
+          chunk=chunk.next
+          next
+        end
+        puts chunk.inner_text.gsub(/^\s*[.]\s*$/, "") if chunk.inner_text && chunk.inner_text != "\n" && chunk.name != 'a'
+      end
+      chunk = chunk.next
+
+    end
+  end
+
   def self.scrape_specific_result(link)
     #DEV
     page = open(link)
     html = Nokogiri::HTML(page)
     result = html.css('a').detect {|n| n['name'] == "#{link.split('#')[1]}"}.parent
-    result.css('.method-callseq').each{|e| puts e.inner_text}
-    puts " "
+    #result.css('.method-callseq').each{|e| puts e.inner_text}
     #still missing the blocks of code!
-    result.css('p').each{|e| puts e.inner_text}
+    result.search('.method-click-advice').each{|e| e.remove}
+    result.search('a').each{|e| e.remove}
+
+    puts " "
+    result.search('.method-callseq').each do |e|
+      puts e.inner_text.colorize(:color => :green)
+    end
+    result.search('.method-callseq').each{|e| e.remove}
+
+    puts " "
+    traverseIt(result)
     puts " "
   end
 
